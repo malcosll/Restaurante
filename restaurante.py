0c61027d3dae2c701642d8bbf4ef7ecd64add939
malcosll <162439457+malcosll@users.noreply.github.com>
2024-05-17 11:25:25 -0300

    restaurante.py

--- restaurante.py
+++ restaurante.py
@@ -5,15 +5,20 @@ class Restaurante:
     def __init__(self,nome,categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'    
     
     def listar_restaurantes():
+        print(f'{"Nome do restaurante"} | {"Categoria"} | {"Status".1just(25)}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.1just(25)} | {restaurante.categoria.1just(25)} | {restaurante.ativo}')
+    
+    @property
+    def ativo(self)
+        return '⌧' if self.ativo else '☐'
     
 
 restaurante_praca = Restaurante('Praça','Gourmet')
